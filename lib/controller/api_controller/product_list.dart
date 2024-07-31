9d0328f258142ee460769b0497533f271e3b5f40
NS NooR <non377938@gmail.com>
2024-07-31 23:24:08 +0600

    loading..3

--- lib/controller/api_controller/product_list.dart
+++ lib/controller/api_controller/product_list.dart
@@ -5,16 +5,16 @@ import 'package:http/http.dart';
 import 'package:learn_api/models/productlist_model.dart';
 
 class ProductListService {
-
-
   static Future<List<Products>> productListService() async {
-    try{
-      Response response = await http.get(Uri.parse("https://cit-ecommerce-codecanyon.bandhantrade.com/api/app/v1/products"));
-      if (response.statusCode==200){
-        ProductListModel data = ProductListModel.fromJson(jsonDecode(response.body));
-       return data.products?? [];
+    try {
+      Response response = await http.get(Uri.parse(
+          "https://cit-ecommerce-codecanyon.bandhantrade.com/api/app/v1/products"));
+      if (response.statusCode == 200) {
+        ProductListModel data =
+            ProductListModel.fromJson(jsonDecode(response.body));
+        return data.products ?? [];
       }
-    }catch (e) {
+    } catch (e) {
       log("error: $e");
     }
     return [];
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'package:learn_api/views/screens/product_list/product_list.dart';
 
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
